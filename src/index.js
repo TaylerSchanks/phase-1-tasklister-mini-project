71690e0ade13b13a42603fd28e72cbafc9c65978
Tayler Schanks <taylerschanks2@gmail.com>
2023-04-07 13:10:37 -0500

    lab objective

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// your code here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', function(e) {
+        e.preventDefault()
+        addToDo(e.target['new-task-description'].value)
+        form.reset()
+    })
+})
+
+function addToDo(toDo) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener("click", deleteToDo)
+    btn.textContent = 'x'
+    p.textContent = `${toDo} `
+    p.appendChild(btn)
+    document.querySelector('#list').appendChild(p)
+
+}
+
+function deleteToDo(e) {
+    e.target.parentNode.remove()
+}
